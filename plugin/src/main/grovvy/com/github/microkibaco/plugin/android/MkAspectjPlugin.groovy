db486b32cd50150573f01d175e83954ae1cf8c00
杨正友 <zhengyouyang.yzy@nonolive.com>
2020-10-08 12:09:43 +0800

    提交MkAspectjPlugin

---
+++ plugin/src/main/grovvy/com/github/microkibaco/plugin/android/MkAspectjPlugin.groovy
@@ -0,0 +1,22 @@
+package com.github.microkibaco.plugin.android
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class MkAspectjPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+        print("This is my first PUGIN")
+
+        project.task('My-Plugin-Task') << {
+
+            print("This is my first Task")
+
+        }
+
+    }
+
+
+}
